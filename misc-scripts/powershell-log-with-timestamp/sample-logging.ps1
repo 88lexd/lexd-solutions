3b19790c77ee4b754752637062daa85c26fc1b6b
Alex Dinh <alex.dinh@hotmail.com>
2021-11-10 12:24:08 +1100

    Fixed my VS Code indent to use 4 spaces

--- misc-scripts/powershell-log-with-timestamp/sample-logging.ps1
+++ misc-scripts/powershell-log-with-timestamp/sample-logging.ps1
@@ -1,23 +1,23 @@
 function WriteLog {
-  param ([String]$Msg)
-  $TimeStamp = Get-Date -Format "dd/MM/yy HH:mm:ss"
-  if ($Global:SaveLog) {
-      if (-not (Test-Path "$Global:LogPath")) {
-          New-Item -ItemType "directory" -Path "$Global:LogPath" -Force -Confirm:$false | Out-Null
-      }
-      $FileTimeStamp = Get-Date -Format "yyyyMMdd"
-      $LogFileWithDate = ($Global:LogFileName.Replace(".log", "_$($FileTimeStamp).log"))
-      Write-Output "[$TimeStamp] $Msg" | Out-File -FilePath "$($Global:LogPath)\$($LogFileWithDate)"  -Append
-  }
-  else {
-      Write-Host "[$TimeStamp] $Msg"
-  }
+    param ([String]$Msg)
+    $TimeStamp = Get-Date -Format "dd/MM/yy HH:mm:ss"
+    if ($Global:SaveLog) {
+        if (-not (Test-Path "$Global:LogPath")) {
+            New-Item -ItemType "directory" -Path "$Global:LogPath" -Force -Confirm:$false | Out-Null
+        }
+        $FileTimeStamp = Get-Date -Format "yyyyMMdd"
+        $LogFileWithDate = ($Global:LogFileName.Replace(".log", "_$($FileTimeStamp).log"))
+        Write-Output "[$TimeStamp] $Msg" | Out-File -FilePath "$($Global:LogPath)\$($LogFileWithDate)"  -Append
+    }
+    else {
+        Write-Host "[$TimeStamp] $Msg"
+    }
 }
 
 function CleanLogs {
-  $Retention = (Get-Date).AddDays(-$($Global:LogRetentionInDays))
-  Get-ChildItem -Path $Global:LogPath -Recurse -Force | Where-Object { $_.Extension -eq ".log" -and $_.CreationTime -lt $Retention } |
-      Remove-Item -Force -Confirm:$false
+    $Retention = (Get-Date).AddDays(-$($Global:LogRetentionInDays))
+    Get-ChildItem -Path $Global:LogPath -Recurse -Force | Where-Object { $_.Extension -eq ".log" -and $_.CreationTime -lt $Retention } |
+        Remove-Item -Force -Confirm:$false
 }
 
 $Global:SaveLog = $true
@@ -29,6 +29,6 @@ WriteLog "Hello World!"
 
 # I always place this at the end of the script to ensure old logs are purged.
 if ($Global:SaveLog) {
-  WriteLog -Msg "Cleaning logs older than $($Global:LogRetentionInDays) days"
-  CleanLogs
+    WriteLog -Msg "Cleaning logs older than $($Global:LogRetentionInDays) days"
+    CleanLogs
 }
