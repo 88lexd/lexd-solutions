ccb52d8feac021082ca0bad3c31f2961c8bb9252
Alex Dinh <alex.dinh@hotmail.com>
2022-01-12 23:53:03 +0000

    SG rules to use for_each loop

--- aws-wordpress/1-infra-configuration/2-general-setup/kubeadm_cluster/security-groups.tf
+++ aws-wordpress/1-infra-configuration/2-general-setup/kubeadm_cluster/security-groups.tf
@@ -79,109 +79,109 @@ resource "aws_security_group" "k8s_workernodes" {
 # Note: The 'aws_security_group_rule' resource cannot be set multiple times for the same SG
 # This method below allow the same resource to contain multiple rules.
 locals {
-  k8s_master_ingress = [
-    {
+  k8s_master_ingress = {
+    allow_ssh_from_jumpbox = {
       description  = "Allow SSH from Jumpbox"
       from_port    = 22
       to_port      = 22
       source_sg_id = var.jumpbox_sg_id
     },
-    {
+    worker_access_kube_apiserver = {
       description  = "Worker to access to kube-apiserver endpoint"
       from_port    = 6443
       to_port      = 6443
       source_sg_id = aws_security_group.k8s_workernodes.id
     },
-    {
+    jumpbox_access_kube_apiserver = {
       description  = "Jumpbox to access kube-apiserver endpoint"
       from_port    = 6443
       to_port      = 6443
       source_sg_id = var.jumpbox_sg_id
     },
-    {
+    worker_access_weavenet = {
       description  = "Access to weavenet"
       from_port    = 6783
       to_port      = 6784
       source_sg_id = aws_security_group.k8s_workernodes.id
     }
-  ]
+  }
 
-  k8s_worker_ingress = [
-    {
+  k8s_worker_ingress = {
+    allow_ssh_from_jumpbox = {
       description  = "Allow SSH from Jumpbox"
       from_port    = 22
       to_port      = 22
       source_sg_id = var.jumpbox_sg_id
       source_cidrs = null
     },
-    {
+    master_to_ingress_controller = {
       description  = "Master to access ingress controller service"
       from_port    = 8443
       to_port      = 8443
       source_sg_id = aws_security_group.k8s_master.id
       source_cidrs = null
     },
-    {
+    master_to_worker_kubelet = {
       description  = "Access to worker kubelet Endpoint"
       from_port    = 10250
       to_port      = 10250
       source_sg_id = aws_security_group.k8s_master.id
       source_cidrs = null
     },
-    {
+    master_to_worker_nodeports = {
       description  = "Access to worker nodeport services"
       from_port    = 30000
       to_port      = 32767
       source_sg_id = aws_security_group.k8s_master.id
       source_cidrs = null
     },
-    {
+    master_to_worker_weavenet = {
       description  = "Access to weavenet"
       from_port    = 6783
       to_port      = 6784
       source_sg_id = aws_security_group.k8s_master.id
       source_cidrs = null
     },
-    {
+    public_http_access = {
       description  = "Allow HTTP traffic to worker nodes"
       from_port    = 80
       to_port      = 80
       source_sg_id = null
       source_cidrs = ["0.0.0.0/0"]
     },
-    {
+    public_https_access = {
       description  = "Allow HTTPS traffic to worker nodes"
       from_port    = 443
       to_port      = 443
       source_sg_id = null
       source_cidrs = ["0.0.0.0/0"]
     }
-  ]
+  }
 }
 
-resource "aws_security_group_rule" "k8s_master_ingress" {
-  count = length(local.k8s_master_ingress)
+resource "aws_security_group_rule" "k8s_master_ingress_rules" {
+  for_each = local.k8s_master_ingress
 
   type                     = "ingress"
-  description              = local.k8s_master_ingress[count.index].description
-  from_port                = local.k8s_master_ingress[count.index].from_port
-  to_port                  = local.k8s_master_ingress[count.index].to_port
+  description              = each.value.description
+  from_port                = each.value.from_port
+  to_port                  = each.value.to_port
   protocol                 = "tcp"
   security_group_id        = aws_security_group.k8s_master.id
-  source_security_group_id = local.k8s_master_ingress[count.index].source_sg_id
+  source_security_group_id = each.value.source_sg_id
 }
 
-resource "aws_security_group_rule" "k8s_worker_kubelet_ingress" {
-  count = length(local.k8s_worker_ingress)
+resource "aws_security_group_rule" "k8s_worker_kubelet_ingress_rules" {
+  for_each = local.k8s_worker_ingress
 
   type                     = "ingress"
-  description              = local.k8s_worker_ingress[count.index].description
-  from_port                = local.k8s_worker_ingress[count.index].from_port
-  to_port                  = local.k8s_worker_ingress[count.index].to_port
+  description              = each.value.description
+  from_port                = each.value.from_port
+  to_port                  = each.value.to_port
   protocol                 = "tcp"
   security_group_id        = aws_security_group.k8s_workernodes.id
-  source_security_group_id = local.k8s_worker_ingress[count.index].source_sg_id != null ? local.k8s_worker_ingress[count.index].source_sg_id : null
-  cidr_blocks              = local.k8s_worker_ingress[count.index].source_cidrs != null ? local.k8s_worker_ingress[count.index].source_cidrs : null
+  source_security_group_id = each.value.source_sg_id != null ? each.value.source_sg_id : null
+  cidr_blocks              = each.value.source_cidrs != null ? each.value.source_cidrs : null
 }
 # End Security groups and rules for K8s instances
 #################################################
