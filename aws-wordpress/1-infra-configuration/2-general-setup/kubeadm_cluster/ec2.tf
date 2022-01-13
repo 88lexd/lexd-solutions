d3cd753dc7142891bcc1cba5343c0831bc549031
Alex Dinh <alex.dinh@hotmail.com>
2022-01-13 01:29:24 +0000

    Changing from Docker to Containerd

--- aws-wordpress/1-infra-configuration/2-general-setup/kubeadm_cluster/ec2.tf
+++ aws-wordpress/1-infra-configuration/2-general-setup/kubeadm_cluster/ec2.tf
@@ -16,48 +16,49 @@ data "aws_ami" "ubuntu" {
 
 # locals will define the instance details for the ec2_instance module
 locals {
-  ec2_instances = [
-    {
-      name  = var.ec2_k8smaster_instance_name
-      ami    = data.aws_ami.ubuntu.id
-      instance_type = var.ec2_k8smaster_instance_type
-      security_group_ids = [aws_security_group.k8s_master.id]
-      subnet_id = var.vpc_public_subnets[0]
-      volume_tags = var.ec2_k8smaster_instance_tags
-      iam_instance_profile = aws_iam_instance_profile.ec2_iam_instance_profile.name
-      tags = var.ec2_k8smaster_instance_tags
-    },
-    {
-      name  = var.ec2_k8sworker_instance_name
-      ami    = data.aws_ami.ubuntu.id
-      instance_type = var.ec2_k8sworker_instance_type
-      security_group_ids = [aws_security_group.k8s_workernodes.id]
-      subnet_id = var.vpc_public_subnets[0]
-      volume_tags = var.ec2_k8sworker_instance_tags
-      iam_instance_profile = aws_iam_instance_profile.ec2_iam_instance_profile.name
-      tags = var.ec2_k8sworker_instance_tags
-    }
-  ]
+  ec2_instances = {
+      k8s_master = {
+        name  = var.ec2_k8smaster_instance_name
+        ami    = data.aws_ami.ubuntu.id
+        instance_type = var.ec2_k8smaster_instance_type
+        security_group_ids = [aws_security_group.k8s_master.id]
+        subnet_id = var.vpc_public_subnets[0]
+        volume_tags = var.ec2_k8smaster_instance_tags
+        iam_instance_profile = aws_iam_instance_profile.ec2_iam_instance_profile.name
+        tags = var.ec2_k8smaster_instance_tags
+      }
+
+      k8s_worker1 = {
+        name  = var.ec2_k8sworker_instance_name
+        ami    = data.aws_ami.ubuntu.id
+        instance_type = var.ec2_k8sworker_instance_type
+        security_group_ids = [aws_security_group.k8s_workernodes.id]
+        subnet_id = var.vpc_public_subnets[0]
+        volume_tags = var.ec2_k8sworker_instance_tags
+        iam_instance_profile = aws_iam_instance_profile.ec2_iam_instance_profile.name
+        tags = var.ec2_k8sworker_instance_tags
+      }
+  }
 }
 
 module "ec2_instance" {
-  count = length(local.ec2_instances)
+  for_each = local.ec2_instances
 
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "3.1.0"
 
-  name = local.ec2_instances[count.index].name
+  name = each.value.name
 
-  ami                    = local.ec2_instances[count.index].ami
-  instance_type          = local.ec2_instances[count.index].instance_type
+  ami                    = each.value.ami
+  instance_type          = each.value.instance_type
   key_name               = var.ec2_keypair_name
-  vpc_security_group_ids = local.ec2_instances[count.index].security_group_ids
-  subnet_id              = local.ec2_instances[count.index].subnet_id
+  vpc_security_group_ids = each.value.security_group_ids
+  subnet_id              = each.value.subnet_id
   enable_volume_tags     = true
-  volume_tags            = local.ec2_instances[count.index].volume_tags
-  iam_instance_profile    = local.ec2_instances[count.index].iam_instance_profile
+  volume_tags            = each.value.volume_tags
+  iam_instance_profile    = each.value.iam_instance_profile
   disable_api_termination = false
-  tags = local.ec2_instances[count.index].tags
+  tags = each.value.tags
 }
 
 # Not using ELB.. therefore only move EIP manually once new worker node is functional!
