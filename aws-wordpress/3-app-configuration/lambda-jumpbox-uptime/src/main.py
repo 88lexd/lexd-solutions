5e04dcd1baafcde976ec90baf0107c414450ffac
Alex Dinh <alex.dinh@hotmail.com>
2022-01-20 12:23:30 +1100

    Test change in main.py

--- aws-wordpress/3-app-configuration/lambda-jumpbox-uptime/src/main.py
+++ aws-wordpress/3-app-configuration/lambda-jumpbox-uptime/src/main.py
@@ -1,5 +1,5 @@
 import sys
 def handler(event, context):
-    print("Hello AWS! Using updated 2022.01.20!")
+    print("Hello AWS! Using updated 2022.01.20! 1")
     print("event = {}".format(event))
     return { 'statusCode': 200 }
