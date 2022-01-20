de22488840dd3dcc0349caad68bcb554bc297a09
Alex Dinh <alex.dinh@hotmail.com>
2022-01-20 13:59:24 +1100

    Use dedicated artifact dir

--- .github/actions/push-object-to-s3/entrypoint.sh
+++ .github/actions/push-object-to-s3/entrypoint.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-pwd
-ls -l
-ls -l /tmp
-
 PROFILE_NAME="push-obj-s3"
 
 # GitHub Actions will prefix env vars with INPUT_ and make them uppercase
@@ -13,4 +9,4 @@ aws configure set aws_access_key_id ${INPUT_AWS_ACCESS_KEY_ID} --profile ${PROFI
 aws configure set aws_secret_access_key ${INPUT_AWS_SECRET_ACCESS_KEY} --profile ${PROFILE_NAME}
 aws configure set region ${INPUT_AWS_REGION} --profile ${PROFILE_NAME}
 
-aws s3 cp ${INPUT_SOURCE_FILE} s3://${INPUT_AWS_S3_BUCKET_NAME}${INPUT_destination_path}/${INPUT_DESTINATION_FILE_NAME} --profile ${PROFILE_NAME}
+aws s3 cp ${INPUT_SOURCE_FILE} s3://${INPUT_AWS_S3_BUCKET_NAME}${INPUT_DESTINATION_PATH}/${INPUT_DESTINATION_FILE_NAME} --profile ${PROFILE_NAME}
