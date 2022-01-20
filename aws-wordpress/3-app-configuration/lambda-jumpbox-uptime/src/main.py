3bdba82a244b0e85b0564699d882b0313f41d337
Alex Dinh <alex.dinh@hotmail.com>
2022-01-20 14:12:27 +1100

    Test trigger

--- aws-wordpress/3-app-configuration/lambda-jumpbox-uptime/src/main.py
+++ aws-wordpress/3-app-configuration/lambda-jumpbox-uptime/src/main.py
@@ -1,5 +1,5 @@
 import sys
 def handler(event, context):
-    print("Hello AWS! Using updated 2022.01.20! 2:09")
+    print("Hello AWS! Using updated 2022.01.20! 2:12")
     print("event = {}".format(event))
     return { 'statusCode': 200 }
