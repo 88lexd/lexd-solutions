41943415cb98033c40d18a151ec5d9d75336c0cf
Alex Dinh <alex.dinh@hotmail.com>
2022-01-24 14:31:23 +1100

    Enable bucket versioning

--- aws-wordpress/1-infra-configuration/2-general-setup/s3.tf
+++ aws-wordpress/1-infra-configuration/2-general-setup/s3.tf
@@ -1,6 +1,24 @@
 resource "aws_s3_bucket" "lambda_s3_bucket" {
   bucket = var.lambda_s3_bucket_name
   acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+    lifecycle_rule {
+    enabled = true
+
+    abort_incomplete_multipart_upload_days = 1
+
+    noncurrent_version_expiration {
+      days = 3
+    }
+
+    expiration {
+      expired_object_delete_marker = true
+    }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "lambda_s3_bucket_block_public" {
