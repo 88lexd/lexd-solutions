78387b3467f9cf0951ad5c0726aacf21be6a2ca2
Alex Dinh <alex.dinh@hotmail.com>
2022-01-24 18:19:34 +1100

    Not using divmod, return decimal value instead

--- aws-wordpress/3-app-configuration/lambda-jumpbox-uptime/src/main.py
+++ aws-wordpress/3-app-configuration/lambda-jumpbox-uptime/src/main.py
@@ -29,7 +29,7 @@ def handler(event, context):
     # Extract timezone info so datetime.now() can match this object for comparision
     timezone = instance.launch_time.tzinfo
     duration_in_seconds = (datetime.now(timezone) - instance.launch_time).total_seconds()
-    last_launch_duration_hours = divmod(duration_in_seconds, 3600)[0]
+    last_launch_duration_hours = round(duration_in_seconds / 3600, 2)
 
     if last_launch_duration_hours > uptime_threshold:
         _message = "-- Executing Stop Instance Action --"
