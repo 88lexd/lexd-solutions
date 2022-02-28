1dcac6ace23de233b2b2067b49648a6995aa9369
Alex Dinh <alex.dinh@hotmail.com>
2022-02-28 11:17:40 +1100

    Minor changeto save 1 line

--- misc-scripts/python-aws-assume-role/bashrc-ps1.sh
+++ misc-scripts/python-aws-assume-role/bashrc-ps1.sh
@@ -1,12 +1,11 @@
-read_credential_file() {
+read_aws_credential_file() {
     python3 <<EOL
 from datetime import datetime
 import os, configparser
-profile = os.getenv("AWS_PROFILE")
 credentials_file = f"{os.getenv('HOME')}/.aws/credentials"
 config = configparser.ConfigParser()
 config.read(credentials_file)
-expiration = config[profile]['expiration']
+expiration = config[os.getenv("AWS_PROFILE")]['expiration']
 expiration_datetime = datetime.fromtimestamp(float(expiration))
 # Time remaining in minutes
 time_remaining = int((expiration_datetime - datetime.now()).total_seconds()/60)
@@ -16,7 +15,7 @@ EOL
 
 role_expiration() {
     if [[ ! -z $AWS_PROFILE ]]; then
-        time_remaining=$(read_credential_file)
+        time_remaining=$(read_aws_credential_file)
         if [[ ${time_remaining} > 0 ]]; then
             echo "[${AWS_PROFILE}|${time_remaining}mins]"
         else
