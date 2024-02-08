444170c09fd8c6148bd27d4e498b4bfdba8b86f0
Alex Dinh <alex.dinh@hotmail.com>
2024-02-08 15:39:39 +1100

    Validated expense key names

--- misc-scripts/python-group-expense-split/main.py
+++ misc-scripts/python-group-expense-split/main.py
@@ -18,8 +18,17 @@ def main():
     print(f"Reading expenses from {expenses_file}")
     all_expenses = read_yaml(expenses_file)
 
+    print("Validating the expense names to match name defined under people...")
+    validated_key_names, bad_names = validate_expenses_names(all_expenses)
+    if not validated_key_names:
+        print('\nError: When setting the names under "expenses", the name must match whats under "people"')
+        print("The following names are not valid (not case sensitive).")
+        [print(f" - {name}") for name in bad_names]
+        print('Double check and ensure the names under expenses match whats defined under "people".')
+        sys.exit(1)
+
     print("Validating the expenses to ensure names are referenced correctly..")
-    validated_names, bad_names = validate_names(all_expenses)
+    validated_names, bad_names = validate_names_for_spitting(all_expenses)
     if not validated_names:
         print('\nError: When using "split_with", the following names do not match whats under "people"')
         print("Not case-sensitive, match is forced to using UPPER CASE.")
@@ -27,18 +36,35 @@ def main():
         print("Double check and ensure the persons name you want to split bill with matches with the people defined.")
         sys.exit(1)
 
-    group = list()
+    print("Building instance for each person...")
+    group_expense = list()
     for person in all_expenses['people']:
-        group.append(Person(
+        group_expense.append(Person(
             name=person,
             expenses=all_expenses['expenses'].get(person, None)
         ))
-    group
+
+    group_expense
+
+
+# Validate names of the key matches under people.
+def validate_expenses_names(all_expenses):
+    all_names_upper = [name.upper() for name in all_expenses['people']]
+
+    bad_names = list()
+    for name, expenses in all_expenses['expenses'].items():
+        if name.upper() not in all_names_upper:
+            bad_names.append(name)
+
+    if len(bad_names) == 0:
+        return True, None
+    else:
+        return False, bad_names
 
 
 # Validate people names and the names referenced for splitting.
 # References must match (UPPER CASE is enforced)
-def validate_names(all_expenses):
+def validate_names_for_spitting(all_expenses):
     all_names_for_splitting = list()
     for name, expenses in all_expenses['expenses'].items():
         result = [v['split_with'] for k,v in expenses.items() if 'split_with' in v.keys()]
